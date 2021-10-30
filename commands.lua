7705eac2aa8f6e950b0f9c8e5366c838b7951524
Iván Gabriel <tocutoeltuco@gmail.com>
2021-10-30 13:11:41 -0300

    Add source code for the bot commands

---
+++ commands.lua
@@ -0,0 +1,50 @@
+-- Source for the commands in the bot (they reference this repo)
+local src = discord.getData("economy-src")
+if src then
+	src = json.decode(src)
+else
+	src = {
+		branches = {},
+		expires = 0,
+	}
+end
+
+if os.time() >= src.expires then
+	local repo = "fiftysol/economy"
+	local head, body = discord.http("https://api.github.com/repos/" .. repo .. "/releases")
+	local releases = json.decode(body)
+
+	local branches = {}
+	local release, branch, asset
+	for i = 1, #releases do
+		release = releases[i]
+		branch = release.target_commitish
+
+		if not branches[branch] then
+			branches[branch] = {}
+			for j = 1, #release.assets do
+				asset = release.assets[j]
+				branches[branch][asset.name] = asset.browser_download_url
+			end
+		end
+	end
+
+	src.branches = branches
+	src.expires = os.time() + 3600
+	discord.saveData("economy-src", json.encode(src))
+end
+
+local branch = "main"
+if not discord.message.isDM then
+	local channel = string.match(discord.message.link, "https://discord.com/channels/%d+/(%d+)")
+	if channel == "474253217421721600" then -- #code-test
+		branch = "develop"
+	end
+end
+
+local head, body = discord.http(src.branches[branch]["dist.lua"])
+local fnc, err = discord.load(body)
+if not fnc then
+	error(err)
+end
+return fnc()
