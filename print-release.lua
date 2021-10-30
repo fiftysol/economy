b17b40b4a448f296bee1870eb5a0e758eaa1d749
Iván Gabriel <tocutoeltuco@gmail.com>
2021-10-30 15:45:09 -0300

    Fix print-release.lua

--- print-release.lua
+++ print-release.lua
@@ -11,19 +11,19 @@ end
 local release = require(path)
 if release.debug then
 	local version = release.version .. os.date(".%Y.%m.%d.%H.%M.%S.debug")
-	local oldVersion = string.gsub(release.version, "%.", "%%%.")
+	local oldVersion = string.gsub(release.version, "%.", "%%.")
 
 	local file = io.open(path .. ".lua")
 	local content = file:read("*a")
 	file:close()
 
 	file = io.open(path .. ".lua", "w")
-	file:write(string.gsub(
+	file:write((string.gsub(
 		content,
 		"(version%s*=%s*[\"'])" .. oldVersion .. "([\"'])",
 		"%1" .. version .. "%2",
 		1
-	))
+	)))
 	file:flush()
 
 	print("::set-output name=version::v" .. version)
